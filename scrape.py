b68a4dd71198f7946def8a5398d8561e35ba5b06
eric00hahn <eric000hahn@gmail.com>
2020-07-21 21:27:35 +0200

    ...

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 from lxml import etree
+import time
 
 def getParagraph(soup) :
   paragraph = soup.find_all('span')
@@ -53,15 +54,33 @@ def writeIndexXML(filename) :
   myfile.write(response.text)
   return None
 
+def getNumberOfParagraphs(soup) :
+  paragraphen = soup.find_all('a')
+  content = ''
+  for p in paragraphen :
+      content += p.text
+  result = [e for e in re.split("[^0-9]", content) if e != '']
+  if len(result) == 0 :
+    return 0
+  else :
+    return max(map(int, result))
+
+def getLinks(soup) :
+  links = soup.find_all('a', href=True)
+  res = []
+  for l in links :
+    res += [l['href']]
+  return res
+
 if __name__ == '__main__' :
 
   url0 = 'https://www.gesetze-im-internet.de/'
-  g = 'bgb'
+  g = 'parkettlmstrv'
 
-  url2 = 'https://www.gesetze-im-internet.de/bgb/__42.html'
-  url3 = 'http://www.gesetze-im-internet.de/kindarbschv/eingangsformel.html'
+  G = getGesetze()
 
-  response = requests.get(url0 + g + '/index.html')
+  response = requests.get(url0 + 'tfg' + '/index.html')
   soup = BeautifulSoup(response.text, 'html')
 
-  print(soup)
+  links = getLinks(soup)
+  print(links)
